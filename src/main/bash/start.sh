27d2f178100ed7f78d8e68326916360d8f89d0dd
alexey-kravtsov <Havok.08@mail.ru>
2017-03-28 18:13:22 +0300

    Updated scripts and config

--- src/main/bash/start.sh
+++ src/main/bash/start.sh
@@ -4,3 +4,6 @@ set -e
 
 cd /opt/socrata-crawler/atsd-data-crawlers/
 mvn exec:java
+cp reports/README.md ../open-data-catalog/
+cp reports/series-commands.md ../open-data-catalog/
+cp -p reports/datasets/* ../open-data-catalog/datasets
--- src/main/bash/update.sh
+++ src/main/bash/update.sh
@@ -5,3 +5,6 @@ set -e
 cd /opt/socrata-crawler/atsd-data-crawlres
 git pull
 mvn clean install
+cp src/main/bash/* ../
+chmod 755 ../start.sh
+chmod 755 ../update.sh
