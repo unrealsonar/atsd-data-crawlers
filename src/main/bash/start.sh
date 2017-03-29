6783e71dd39e7dc96ce117aed5823e3fcb2244d7
alexey-kravtsov <Havok.08@mail.ru>
2017-03-29 10:06:27 +0300

    Updated start script

--- src/main/bash/start.sh
+++ src/main/bash/start.sh
@@ -7,3 +7,11 @@ mvn exec:java
 cp reports/README.md ../open-data-catalog/
 cp reports/series-commands.md ../open-data-catalog/
 cp -p reports/datasets/* ../open-data-catalog/datasets
+cd ../open-data-catalog/
+. /opt/socrata-crawler/atsd-data-crawlers/src/main/resources/data.properties
+git config user.name $github_username
+git config user.email $github_email
+git remote set-url origin https://$github_username:$github_password@github.com/axibase/open-data-catalog
+git add --all
+git commit -m "Updated dataset files"
+git push origin docker-test
